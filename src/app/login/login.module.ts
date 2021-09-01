7a204c46af792ddc265bae5020c4f5b647fad6e4
Ariel Pons <ariel_pons@hotmail.com>
2021-09-01 16:15:14 -0300

    Módulos y ruteo agregado

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoginRoutingModule } from './login-routing.module';
 
 import { SignupComponent } from './signup/signup.component';
 import { SigninComponent } from './signin/signin.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -13,6 +16,9 @@ import { ReactiveFormsModule } from '@angular/forms';
   ],
   imports: [
     CommonModule,
+    LoginRoutingModule,
+    SharedModule,
+    RouterModule,
     ReactiveFormsModule
   ],
   exports: [

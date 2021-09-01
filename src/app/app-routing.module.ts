d9bf6cb84ac337d94ceca7124eda69a85b74d4e4
Ariel Pons <ariel_pons@hotmail.com>
2021-09-01 16:10:47 -0300

    Ruteo arreglado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { AgregarRopaComponent } from './components/productos/agregar-ropa/agregar-ropa.component';
 import { LoggedGuard } from './logged.guard';
 import { NotFoundComponent } from './shared/not-found/not-found.component';
 
@@ -10,7 +11,8 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'error', component: NotFoundComponent},
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
-  { path: 'productos', canLoad: [LoggedGuard],loadChildren: () => import('./components/productos/productos.module').then(m => m.ProductosModule) },
+  { path: 'productos', canLoad:[LoggedGuard], loadChildren: () => import('./components/productos/productos.module').then(m => m.ProductosModule) },
+  {path: 'agregar', canLoad:[LoggedGuard], component:AgregarRopaComponent},
   { path: "**", component: NotFoundComponent}
 ];
 
--- src/app/components/productos/productos-routing.module.ts
+++ src/app/components/productos/productos-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgregarRopaComponent } from './agregar-ropa/agregar-ropa.component';
 import { RopaComponent } from './ropa/ropa.component';
 import { RopasComponent } from './ropas/ropas.component';
 
 const routes: Routes = [
   { path: '', component: RopasComponent },
   { path: ':id', component: RopaComponent },
+  { path: 'agregar', component:AgregarRopaComponent},
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 

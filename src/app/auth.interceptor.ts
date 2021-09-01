fa8f50054071eabb28d0fe9940525add10e419cc
Ariel Pons <ariel_pons@hotmail.com>
2021-09-01 16:11:17 -0300

    Interceptor agregado

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -13,15 +13,10 @@ export class AuthInterceptor implements HttpInterceptor {
 
   constructor(private auth: UsersService) {}
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if (
-      req.url === 'http://localhost:3000/auth/register' ||
-      req.url === 'http://localhost:3000/auth/login'
-    ) {
+    if (req.url === 'http://localhost:3000/api/auth/') {
       return next.handle(req);
     } else {
       const authHeader = `Bearer ${this.auth.tokenLogin}`;
-
-      //CLONAMOS LA REQUEST Y LE AGREGAMOS EL HEADER CON LA AUTHORIZATION
       const authReq = req.clone({
         headers: req.headers.set('Authorization', authHeader),
       });
